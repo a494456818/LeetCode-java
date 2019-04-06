502a71d7b3f87bcf2e1285751bae4a486b3ca09c
ingZT <1179392739@qq.com>
2019-04-06 09:28:00 +0800

    Create Solution.java

---
+++ 9/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
